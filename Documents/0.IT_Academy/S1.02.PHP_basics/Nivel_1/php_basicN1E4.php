85dbba1206b3d74f97fd5fe56232b483ca93304a
Asier Carrasco González <axiercarrasco@gmail.com>
2025-06-02 12:38:31 +0200

    removed unuseful comments

--- Documents/0.IT_Academy/S1.02.PHP_basics/Nivel_1/php_basicN1E4.php
+++ Documents/0.IT_Academy/S1.02.PHP_basics/Nivel_1/php_basicN1E4.php
@@ -1,20 +1,20 @@
 <?php
 function countingApp($endNumber = 10, $countingIncrements = 1) {
-    $currentNumber = 0; // Initialize the starting number
+    $currentNumber = 0;
 
         while ($currentNumber + $countingIncrements <= $endNumber) {
-            echo $currentNumber . ", "; // Print the current number
-            $currentNumber += $countingIncrements; // Increment the current number by the specified counting increments
+            echo $currentNumber . ", ";
+            $currentNumber += $countingIncrements;
         }
 //As the programme does not count until exactly the end number, we need to print the end number if it was not reached in the loop.
         if($currentNumber != $endNumber) {
-            echo $endNumber . "."; // Print the end number if it was not reached in the loop
+            echo $endNumber . ".";
         } else {
             echo $currentNumber . ".";
         }
     }
 
-countingApp(50, 6); // Call the function with example values
+countingApp(50, 6);
 echo "<br>";
 
 countingApp(6);
